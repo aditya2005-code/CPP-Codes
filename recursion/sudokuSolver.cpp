27514e7aac3c63c75f803aaf54bf69d4b42fdf84
aditya2005-code <2k23.csds2312133@gmail.com>
2025-09-09 18:53:55 +0530

    ADD Sudoku Solver Cpp File

---
+++ recursion/sudokuSolver.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isSafe(board, row, col, ch)) {
+                            board[row][col] = ch;
+                            if (solve(board)) return true;
+                            board[row][col] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isSafe(vector<vector<char>>& board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) return false;
+            if (board[i][col] == ch) return false;
+        }
+        int startRow = row - row % 3, startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == ch) return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+    Solution().solveSudoku(board);
+    for (auto& row : board) {
+        for (auto& c : row) cout << c << " ";
+        cout << "\n";
+    }
+}
